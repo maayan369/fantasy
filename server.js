1524c31044426a0ba47e62f48f4a06fb2d769cc3
maayan369 <131189255+maayan369@users.noreply.github.com>
2023-11-08 21:17:54 +0200

    שימוש בסוקט שני שליש

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser } = require('./utils/users');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server); 
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatCord Bot'
+
+// Run when client connects
+io.on('connection', socket => {
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        console.log(username, room);
+
+        socket.join(user.room);
+
+        // welcome current user
+        socket.emit('message', formatMessage(botName, 'welcome to chatcord!'));
+
+        // broadcast when a user connects
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName, `${user.username} has joined the chat`));
+
+    });
+ 
+    // listen for chatMessage
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', formatMessage('USER', msg));
+    });
+
+    // run when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'a user has left the chat'));
+    });
+
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
