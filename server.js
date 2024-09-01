67ba91a2b7ace8efbaebcc930dcc892dd1b28289
maayan369 <131189255+maayan369@users.noreply.github.com>
2024-09-01 12:39:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -398,7 +398,7 @@ const LOCAL_IP = '192.168.0.103';
 
 const PORT = process.env.PORT || 3000;
 
-// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // also change at signup and login js
 
 server.listen(PORT, LOCAL_IP, () => {
